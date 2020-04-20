2f95a1ba081f15f1dd58dc273e86f6b443f59dca
Yannick.Daelemans <yannick.daelemans@student.kuleuven.be>
2020-04-20 17:20:53 +0200

    made the initials for the COM port reading Still have an error when running at line 33

--- src/COMReader.java
+++ src/COMReader.java
@@ -9,13 +9,13 @@ public class COMReader implements Runnable, SerialPortEventListener{
     SerialPort serialPort;
     Thread readThread;
 
-    private COMReader(){
+    public COMReader(){
         Enumeration<CommPortIdentifier> portEnum = CommPortIdentifier.getPortIdentifiers();
 
         while(portEnum.hasMoreElements()){
             CommPortIdentifier currPortId = portEnum.nextElement();
 
-            if(currPortId.getName().equals("COM11")){   //port on this Windows computer
+            if(currPortId.getName().equals("COM11")){   // port on this Windows computer
                 if(currPortId.getPortType() == CommPortIdentifier.PORT_SERIAL){
                     portId = currPortId;                // set the portId
                 }else{
@@ -27,13 +27,70 @@ public class COMReader implements Runnable, SerialPortEventListener{
         }
     }
 
+    public void StartReadingThread(){
+        // open the COM port
+        try{
+            serialPort = (SerialPort) portId.open("COMReader", 2000);
+        } catch (PortInUseException e){
+            System.out.println(e);
+        }
+        // start up the inputStream from the device to the computer
+        try {
+            inputStream = serialPort.getInputStream();
+        }catch (IOException e){
+            System.out.println(e);
+        }
+        // add the event listener to the port
+        try {
+            serialPort.addEventListener(this);
+        }catch (TooManyListenersException e){
+            System.out.println(e);
+        }
+        // set parameters for the serial port
+        try {
+            serialPort.setSerialPortParams(9600,
+                    SerialPort.DATABITS_8,
+                    SerialPort.STOPBITS_1,
+                    SerialPort.PARITY_NONE);
+        } catch (UnsupportedCommOperationException e) {
+            System.out.println(e);
+        }
+        //start the reading thread
+        readThread = new Thread(this);
+        readThread.start();
+    }
+
     @Override
     public void run() {
-
+        try {
+            Thread.sleep(20000);
+        } catch (InterruptedException e) {System.out.println(e);}
     }
 
     @Override
     public void serialEvent(SerialPortEvent serialPortEvent) {
+        switch(serialPortEvent.getEventType()) {
+            case SerialPortEvent.BI:
+            case SerialPortEvent.OE:
+            case SerialPortEvent.FE:
+            case SerialPortEvent.PE:
+            case SerialPortEvent.CD:
+            case SerialPortEvent.CTS:
+            case SerialPortEvent.DSR:
+            case SerialPortEvent.RI:
+            case SerialPortEvent.OUTPUT_BUFFER_EMPTY:
+                break;
+            case SerialPortEvent.DATA_AVAILABLE:
+                byte[] readBuffer = new byte[20];
 
+                try {
+                    while (inputStream.available() > 0) {
+                        int numBytes = inputStream.read(readBuffer);
+                    }
+                    System.out.print(new String(readBuffer));
+                } catch (IOException e) {System.out.println(e);}
+                break;
+        }
     }
+
 }
--- src/main.java
+++ src/main.java
@@ -2,6 +2,7 @@ public class main {
 
     public static void main(String[] args) {
         COMReader comReader = new COMReader();
+        comReader.StartReadingThread();
     }
 
 }
